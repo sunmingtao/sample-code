2e985aae2ed78002988d2f1e72ce868d3907e5f0
Mingtao SUN <Mingtao SUN>
2016-03-27 14:32:08 +1100

    format code

--- badminton/src/main/java/badminton/badminton/Court.java
+++ badminton/src/main/java/badminton/badminton/Court.java
@@ -4,33 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Court {
-	private List<Player> players = new ArrayList<Player>();
-	
-	private int id;
-		
-	public Court(int id) {
-		super();
-		this.id = id;
-	}
+    private List<Player> players = new ArrayList<Player>();
 
-	public int getId() {
-		return id;
-	}
+    private int id;
 
-	public void addPlayer(Player player){
-		this.players.add(player);
-	}
-	
-	public void addPlayers(List<Player> players){
-		this.players.addAll(players);
-	}
-	
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		sb.append("Court ID: ").append(id).append("\n");
-		for (Player player: players){
-			sb.append(player.toString()).append("\n");
-		}
-		return sb.toString();
-	}
+    public Court(int id) {
+        super();
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addPlayer(Player player) {
+        this.players.add(player);
+    }
+
+    public void addPlayers(List<Player> players) {
+        this.players.addAll(players);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Court ID: ").append(id).append("\n");
+        for (Player player : players) {
+            sb.append(player.toString()).append("\n");
+        }
+        return sb.toString();
+    }
 }
--- badminton/src/main/java/badminton/badminton/Player.java
+++ badminton/src/main/java/badminton/badminton/Player.java
@@ -1,31 +1,37 @@
 package badminton.badminton;
 
 public class Player {
-	private int id;
-	/** Number of games played */
-	private int gameNumberPlayed;
-	
-	public Player(int id) {
-		super();
-		this.id = id;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getCount() {
-		return gameNumberPlayed;
-	}
-	public void incrementGameNumberPlayed() {
-		this.gameNumberPlayed++;
-	}
-	
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		sb.append("ID: ").append(id).append("  ");
-		sb.append("Number of games played: ").append(gameNumberPlayed);
-		return sb.toString();
-	}
+    private int id;
+    /**
+     * Number of games played
+     */
+    private int gameNumberPlayed;
+
+    public Player(int id) {
+        super();
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCount() {
+        return gameNumberPlayed;
+    }
+
+    public void incrementGameNumberPlayed() {
+        this.gameNumberPlayed++;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ID: ").append(id).append("  ");
+        sb.append("Number of games played: ").append(gameNumberPlayed);
+        return sb.toString();
+    }
 }
