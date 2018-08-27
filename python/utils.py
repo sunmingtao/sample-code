b56adf427977f28dd2b5c57fcbcbb7134ac1bd0f
Mingtao Sun <msun@mac-29863.shire.nla.gov.au>
2018-08-27 11:54:35 +1000

    added two utils functions

--- python/utils.py
+++ python/utils.py
@@ -1,2 +1,23 @@
 import numpy as np
 
+'''Convert 'ABC' to [[1,0,0],[0,1,0],[0,0,1]] '''
+def string_to_one_hot_vector(string, known_chars='ABCDEFG'):
+    value_to_index_dict = {value: index for index, value in enumerate(known_chars)} # {'A':0, 'B':1, 'C':2}
+    output = np.zeros((len(string), len(known_chars)), dtype=np.int32)
+    for i, value in enumerate(string):
+        output[i, value_to_index_dict[value]] = 1
+    return output
+
+
+'''Randomly replace a char in a string. e.g. 'ABCDE' -> 'ABBDE' '''
+def replace_char_in_string(string):
+    index = np.random.randint(len(string))
+    old_char = string[index]
+    new_char_list = sorted(set(string) - set(old_char)) # sorted list
+    new_char = np.random.choice(new_char_list)
+    return string[:index]+ new_char + string[index+1:]
+
+
+
+
+
