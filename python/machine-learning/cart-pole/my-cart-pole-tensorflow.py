c3246e8f19bee1878554602496e66359c104accd
Mingtao Sun <msun@mac-29863.shire.nla.gov.au>
2018-09-05 09:11:15 +1000

    move files

---
+++ python/machine-learning/cart-pole/my-cart-pole-tensorflow.py
@@ -0,0 +1,14 @@
+import gym
+import matplotlib
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import os
+
+env = gym.make("CartPole-v0")
+
+max_steps = 10000
+
+def q_network(X_state, name):
+    pass
